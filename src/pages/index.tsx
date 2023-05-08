3a4f09d89bca83f60b04a03d54562e2afaf1f351
Erick Sarabia <erick@clappy.io>
2023-05-08 11:38:36 -0600

    feat(home): add grid and stack items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,101 +16,122 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={`${styles.header} ${inter.className}`}>
+        <nav className={styles.navigation}>
+          <Link href="/">Home</Link>
+          <Link href="/">Products</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">Recipes</Link>
+        </nav>
+      </header>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <section className={styles.grid}>
+          <div className={styles.featured}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={600}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>
+                Faucibus interdum posuere lorem ipsum dolor sit amet
+                consectetur. Ipsum dolor sit amet consectetur adipiscing elit ut
+                aliquam purus.
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+          <div className={styles.first}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={230}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>
+                Faucibus interdum posuere lorem ipsum dolor sit amet
+                consectetur.
+              </p>
+            </div>
+          </div>
+          <div className={styles.second}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={230}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>
+                Ipsum dolor sit amet consectetur adipiscing elit ut aliquam
+                purus.
+              </p>
+            </div>
+          </div>
+          <div className={styles.third}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={230}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>
+                Faucibus interdum posuere lorem ipsum dolor sit amet
+                consectetur.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className={styles.stack}>
+          <div className={styles.card}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={372}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor</h3>
+              <p>
+                Faucibus interdum posuere lorem ipsum dolor sit amet
+                consectetur.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={372}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor</h3>
+              <p>
+                Faucibus interdum posuere lorem ipsum dolor sit amet
+                consectetur.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img
+              alt="placeholder"
+              src="https://via.placeholder.com/1024x768/eee?text=4:3"
+              width={372}
+            ></img>
+            <div className={styles.text}>
+              <h3>Lorem ipsum dolor</h3>
+              <p>
+                Ipsum dolor sit amet consectetur adipiscing elit ut aliquam
+                purus.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
+      <footer className={`${styles.footer} ${inter.className}`}>
+        © 2023 - All rights reserved.
+      </footer>
     </>
-  )
+  );
 }
